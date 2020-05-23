f007067cd516133123414727e7ceadd6c5c82a10
JAYANTA GOGOI <jayantagogoi@McCloskey.local>
2020-05-23 17:21:11 +0530

    Ommit private credential

--- configs/appConst.js
+++ configs/appConst.js
@@ -1,4 +1,4 @@
 exports.MONGODB_URI =
-  "mongodb+srv://albert:19Albert84@cluster0-l3k2e.mongodb.net/spotify_example";
+  "*********************************************/spotify_example";
 
 exports.APP_KEY = "my_app_key";
